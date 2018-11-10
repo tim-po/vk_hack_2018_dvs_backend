abcc8c2bf494e88406765a8632fd4dcd79051bdd
Dmitry <boldin.dmitry@gmail.com>
2018-11-10 12:09:59 +0300

    Kostil for Serega was added.

--- vk_hack_2018/content/views.py
+++ vk_hack_2018/content/views.py
@@ -8,19 +8,31 @@ from .serializers import EventSerializer, NewsSerializer
 
 class EventsInfo(APIView):
     def get(self, request):
-        last_ten_events = Event.objects.all().order_by('-id')[:5]
+        last_ten_events = Event.objects.all().order_by('-id')[:10]
 
         serializer = EventSerializer(reversed(last_ten_events), many=True)
-        json_data = JsonResponse(serializer.data, safe=False)
+        serializer_data = serializer.data
+
+        for i, event in enumerate(serializer_data):
+            serializer_data[i]["dates"]["time_from"] = event["dates"]["time_from"].replace("T", " ").replace("Z", "")
+            serializer_data[i]["dates"]["time_to"] = event["dates"]["time_to"].replace("T", " ").replace("Z", "")
+
+        json_data = JsonResponse(serializer_data, safe=False)
 
         return json_data
 
 
 class NewsInfo(APIView):
     def get(self, request):
-        last_ten_news = News.objects.all().order_by('-id')[:5]
+        last_ten_news = News.objects.all().order_by('-id')[:10]
 
         serializer = NewsSerializer(reversed(last_ten_news), many=True)
-        json_data = JsonResponse(serializer.data, safe=False)
+
+        serializer_data = serializer.data
+
+        for i, news in enumerate(serializer_data):
+            serializer_data[i]["date"] = news["date"].replace("T", " ").replace("Z", "")
+
+        json_data = JsonResponse(serializer_data, safe=False)
 
         return json_data
